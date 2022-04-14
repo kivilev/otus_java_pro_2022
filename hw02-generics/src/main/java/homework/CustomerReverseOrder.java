6f3ca1b7fe456fd76441fe9827621ef5a24ade23
d.kivilev <d.kivilev@qiwi.com>
2022-04-14 12:59:18 +0300

    Fix some minor issues

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -9,16 +9,13 @@ public class CustomerReverseOrder {
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
     //Пояснение: заюзал LL т.к. нет никакой сортировки и он имплементирует интерфейс Queue с удобным методом pollLast.
-    //Deque<Customer> linkedList = new LinkedList<>();
     private final Deque<Customer> queue = new ArrayDeque<>();
 
     public void add(Customer customer) {
-        //linkedList.add(customer);
         queue.push(customer);
     }
 
     public Customer take() {
-        // return linkedList.pollLast();
         return queue.pop();
     }
 }
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,6 +1,7 @@
 package homework;
 
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
@@ -9,7 +10,7 @@ public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final NavigableMap<Customer, String> treeMap = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
+    private final NavigableMap<Customer, String> treeMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
