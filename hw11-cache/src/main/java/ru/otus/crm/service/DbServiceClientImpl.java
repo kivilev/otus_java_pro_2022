271f6647092bc76ec418380c3422587a9804c04d
Denis Kivilev <25749197+kivilev@users.noreply.github.com>
2022-06-30 16:47:13 +0300

    Homework 11 - Cache for DB  (#11)
    * Initial commit
    
    * cache part
    
    * Fix problem with cache when object wasn't founded
    
    Co-authored-by: d.kivilev <d.kivilev@qiwi.com>

---
+++ hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -0,0 +1,70 @@
+package ru.otus.crm.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.sessionmanager.TransactionRunner;
+import ru.otus.crm.model.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DbServiceClientImpl implements DBServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+
+    private final DataTemplate<Client> dataTemplate;
+    private final TransactionRunner transactionRunner;
+    private final HwCache<String, Client> cache = new MyCache<>();
+
+    public DbServiceClientImpl(TransactionRunner transactionRunner, DataTemplate<Client> dataTemplate) {
+        this.transactionRunner = transactionRunner;
+        this.dataTemplate = dataTemplate;
+    }
+
+    @Override
+    public Client saveClient(Client client) {
+        return transactionRunner.doInTransaction(connection -> {
+            if (client.getId() == null) {
+                var clientId = dataTemplate.insert(connection, client);
+                var createdClient = new Client(clientId, client.getName());
+                log.info("created client: {}", createdClient);
+
+                cache.put(String.valueOf(clientId), createdClient);
+
+                return createdClient;
+            }
+
+            cache.remove(String.valueOf(client.getId()));
+            cache.put(String.valueOf(client.getId()), client);
+
+            dataTemplate.update(connection, client);
+            log.info("updated client: {}", client);
+            return client;
+        });
+    }
+
+    @Override
+    public Optional<Client> getClient(long id) {
+        return transactionRunner.doInTransaction(connection -> {
+            var clientOptional = Optional.ofNullable(cache.get(String.valueOf(id)));
+            if (clientOptional.isEmpty()) {
+                clientOptional = dataTemplate.findById(connection, id);
+                clientOptional.ifPresent(client -> cache.put(String.valueOf(id), client));
+
+            }
+            log.info("client: {}", clientOptional);
+            return clientOptional;
+        });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return transactionRunner.doInTransaction(connection -> {
+            var clientList = dataTemplate.findAll(connection);
+            log.info("clientList:{}", clientList);
+            return clientList;
+        });
+    }
+}
